d3c6a277bf9f5ff1cf1c2717b73208b8d2717c42
yoonkt200@gmail.com <yoon@yungitaeui-MacBook-Pro.local>
2017-07-05 12:00:08 +0900

    data preprocessing end

--- R_work/day3.R
+++ R_work/day3.R
@@ -36,9 +36,80 @@ yy <- data.frame(name2=c("c", "b", "a"), english=c(4,5,6))
 merge(xx, yy)
 merge(xx, yy, all=T, by.x='name1', by.y='name2')
 
-# subset & set factor
+### subset & set factor
 s1 = subset(iris, Species == "setosa")
 str(s1)
 str(iris)
 s1$Species = factor(s1$Species)
 str(s1)
+
+### species를 기준으로 iris데이터를 리스트로 쪼갬
+s2 = split(iris, iris$Species)
+str(s2)
+
+### sort, order
+s3 = c(1,5,3,7,9,2)
+sort(s3, decreasing = F)
+order(s3, decreasing = F) # current index of sorted data
+
+### remove NA data
+mean(c(1:5, NA), na.rm = T) # NA data remove
+
+### find location
+k = c(2,4,6,7,10)
+which(k %% 2 == 0) # return index
+
+### apply function
+d <- matrix(1:9, ncol=3)
+mean(d)
+mean(d[1,])
+for (i in 1:nrow(d)){
+  print(mean(d[i,]))
+}
+
+apply(d, 1, sum) # 1 : by row
+apply(d, 2, sum) # 2 : by column (attributes = features)
+
+# lapply : list return
+# sapply : vector return
+# tapply : group processing
+
+tapply(iris$Sepal.Length, iris$Species, sum)
+
+list_data1 <- list(a = 1:5, b = 6:10)
+mapply(sum, list_data1$a, list_data1$b)
+
+### doBy
+# install.packages("doBy")
+library(doBy)
+?summaryBy
+summary(iris)
+summaryBy(.~Species, iris) # 모든 attribute를 species를 기준으로 요약
+summaryBy(Sepal.Length~Species, iris, FUN = median) # Sepallegnth를 species 기준으로 요약, median func로.
+# == aggregate(Sepal.Length~Species, iris, FUN = median)
+
+sample(1:20, 200, replace = T, prob = c(1,3,3, rep(1,17))) # 2,3이 많이 뽑힌다.
+sampleBy(Sepal.Length~Species, data = iris, frac = 0.1, systematic = T) # 종 별로 10%씩 샘플링
+
+### stack, unstack example
+### melt example
+
+### RMySQL package : pass
+
+### sqldf package
+# install.packages("sqldf")
+dyn.load("/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/jre/lib/server/libjvm.dylib")
+library(rJava)
+library(sqldf)
+sqldf("SELECT DISTINCT Species 
+      FROM iris")
+iris_rename = iris
+names(iris_rename) = c("SL", "SW", "PL", "PW", "SP")
+sqldf("SELECT AVG(SL) 
+      FROM iris_rename
+      WHERE SP='setosa'")
+
+library("MASS")
+data("survey")
+str(survey)
+View(survey)
--- week2_StatisticAnalysis/day3.md
+++ week2_StatisticAnalysis/day3.md
@@ -55,19 +55,87 @@ yy <- data.frame(name2=c("c", "b", "a"), english=c(4,5,6))
 merge(xx, yy)
 merge(xx, yy, all=T, by.x='name1', by.y='name2')
 
-# subset & set factor
+### subset & set factor
 s1 = subset(iris, Species == "setosa")
 str(s1)
 str(iris)
 s1$Species = factor(s1$Species)
 str(s1)
 
+### species를 기준으로 iris데이터를 리스트로 쪼갬
+s2 = split(iris, iris$Species)
+str(s2)
+
+### sort, order
+s3 = c(1,5,3,7,9,2)
+sort(s3, decreasing = F)
+order(s3, decreasing = F) # current index of sorted data
+
+### remove NA data
+mean(c(1:5, NA), na.rm = T) # NA data remove
+
+### find location
+k = c(2,4,6,7,10)
+which(k %% 2 == 0) # return index
+
+```
+
+> **1.3 apply 함수**
+
+- 주로 array나 matrix 데이터 구조를 처리하기에 용이함.
+- row나 column 단위의 연산을 할 때.
+- lapply, sapply 가 제일 많이 쓰이지만 최신버전의 R 에서는 내장되어있기 때문에 코딩할 일은 별로 없다.
+- tapply, mapply 등의 특수한 apply 함수를 유용하게 사용해야 한다.
+
+```R
+
+### apply function
+d <- matrix(1:9, ncol=3)
+mean(d)
+mean(d[1,])
+for (i in 1:nrow(d)){
+  print(mean(d[i,]))
+}
+
+apply(d, 1, sum) # 1 : by row
+apply(d, 2, sum) # 2 : by column (attributes = features)
+
+# lapply : list return
+# sapply : vector return
+# tapply : group processing
+
+tapply(iris$Sepal.Length, iris$Species, sum)
+
+list_data1 <- list(a = 1:5, b = 6:10)
+mapply(sum, list_data1$a, list_data1$b)
+
+```
+
+> **1.4 doBy**
+
+
+```R
+
+### doBy
+# install.packages("doBy")
+library(doBy)
+?summaryBy
+summary(iris)
+summaryBy(.~Species, iris) # 모든 attribute를 species를 기준으로 요약
+summaryBy(Sepal.Length~Species, iris, FUN = median) # Sepallegnth를 species 기준으로 요약, median func로.
+# == aggregate(Sepal.Length~Species, iris, FUN = median)
+
+sample(1:20, 200, replace = T, prob = c(1,3,3, rep(1,17))) # 2,3이 많이 뽑힌다.
+sampleBy(Sepal.Length~Species, data = iris, frac = 0.1, systematic = T) # 종 별로 10%씩 샘플링
+
 ```
 
 -----------------------
 
 
-#### **2. 베이즈 정리**
+#### **3. 집단간 차이검정**
+
+- t 검정
 
 ```
 
