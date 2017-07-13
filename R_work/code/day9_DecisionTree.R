53d93d242f83c9be1cfa535087a86227961285f4
yoonkt200 <yoonkt200@gmail.com>
2017-07-13 17:37:29 +0900

    'asdf'

--- R_work/code/day9_DecisionTree.R
+++ R_work/code/day9_DecisionTree.R
@@ -33,6 +33,7 @@ table(test$Class, pred2)
 sum(test$Class==pred2)/nrow(test)
 # tree depth는 3~4를 벗어나면 성능이 떨어질 수 있음.
 
+### pruning
 install.packages("tree")
 library(tree)
 library(MASS)
@@ -40,6 +41,9 @@ library(MASS)
 ir.tr = tree(Class~., train) # 가지치기 전
 plot(ir.tr)
 text(ir.tr, all = T)
+
+plot(prune.misclass(ir.tr)) 
+# 트리의 depth가 적당하면서 misclass가 낮은 지점은 10~15 사이.
 
 ir.tr1 = prune.misclass(ir.tr, best=14) # 가지치기 후
 plot(ir.tr1)
