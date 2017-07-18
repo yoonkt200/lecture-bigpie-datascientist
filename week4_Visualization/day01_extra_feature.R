3113c31d5652675110795073c2e088bb1e2ceb26
yoonkt200@gmail.com <yoon@yungitaeui-MacBook-Pro.local>
2017-07-18 12:31:32 +0900

    index rename

--- week4_Visualization/day01_extra_feature.R
+++ week4_Visualization/day01_extra_feature.R
@@ -3,3 +3,10 @@ dyn.load("/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/jre/l
 library(rJava)
 Sys.setlocale("LC_ALL", "ko_KR.UTF-8") # 한글 인코딩 가능하게 해줌
 
+### 그림 덮어쓰기와 그림 삭제
+plot(rnorm(1:10), col = "red")
+par(new=T) # 그림 이어쓰기
+plot(rnorm(1:10), col = "blue")
+par(new=T)
+plot.new() # 그림 삭제
+
